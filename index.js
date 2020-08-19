021681bb36cd5c5e016ee211c2e7259bbae62a19
Ludwig <ludwig.henriques@gmail.com>
2020-08-19 23:15:10 +0200

    FO: intégration services et compétences

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ $(function(){
     $("body a").on("click", function(event){
         event.preventDefault();
         var hash = this.hash;
+        console.log(hash)
 
-        $('body, html').animate({scrollTop: $(hash).offset().top}, 900, function(){
-            window.location.hash = hash;
+        $('body, html').animate({scrollTop: ($(hash).offset().top) - 55}, 900, function(){
+            // window.location.hash = hash;
         });
     });
 })
